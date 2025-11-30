abe292ef1ea7e908b582a1573688d668769d5e2e
azhyhg <azhy1417@gmail.com>
2025-11-29 20:21:21 -0800

    Change minimum API to 29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "coolio.zoewong.traverse"
-        minSdk = 26
+        minSdk = 29
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
