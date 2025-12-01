76755f6b172cf4b30836b4b39a0e2ec8a75deeea
Zoe Wong <125703675+zswong@users.noreply.github.com>
2025-12-01 13:22:32 -0800

    Update Theme.kt

--- app/src/main/java/coolio/zoewong/traverse/ui/theme/Theme.kt
+++ app/src/main/java/coolio/zoewong/traverse/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -20,7 +21,11 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40 ,
+    primaryContainer = Color(0xFF9ECCFA),
+    secondaryContainer = Color(0xFF2D69FF),
+
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -36,7 +41,7 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun TraverseTheme(
     darkTheme: Boolean = ThemeManager.isDarkMode,
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
